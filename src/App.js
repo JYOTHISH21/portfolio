f8667453b5c26862dd64dd35ec0bb0cba21056a2
JYOTHISH21 <jyothish2108@gmail.com>
2023-06-19 12:55:55 +0530

    Deployed website

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
   return (
     <>
       <Routes>
-        <Route path="/" element={<Layout/>}>
+        <Route path="/portfolio" element={<Layout/>}>
           <Route index element={<Home/>}/>
           <Route path='about' index element={<About/>}/>
           <Route path='contact' index element={<Contact/>}/>
